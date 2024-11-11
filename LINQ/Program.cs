30ade0b183ae3a7a1ca5bdf85aaf6e4a5c3b5a0f
zach brecht <brechtz32@gmail.com>
2024-11-11 16:57:27 -0600

    first commit for LINQExcercise

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace LINQ
+﻿using System;
+using System.Collections.Generic;
+using LINQ;
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var myVideoGames = new List<string>() { "call of duty", "league of legends", "raid shadow legens", "clash of clans"};
+
+            var myGames = myVideoGames.OrderBy(x => x);
+
+            foreach (var x in myGames)
+            {
+                Console.WriteLine(x);
+            }
+            
         }
     }
 }
